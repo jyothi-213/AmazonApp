cf1f48af7a92366e7ce09bc12fbc7c0eb8189228
Venu-Pooji-Jyothi <Venu-Pooji-Jyothi@GANESHA>
2023-07-25 19:25:54 +0530

    Add project files.

---
+++ AmazonWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using AmazonWebApp.Data;
+using AmazonWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmazonWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AmazonDbContext _db;
+
+        public CategoryController(AmazonDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategories=_db.Categories.ToList();
+            return View(objCategories);
+        }
+    }
+}
---
+++ AmazonWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmazonWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
